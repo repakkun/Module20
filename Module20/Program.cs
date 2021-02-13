42a8014402b113c2abe9a1b6bf2502f924770caf
repakkun <58113303+repakkun@users.noreply.github.com>
2021-02-13 13:05:35 +0300

    Add files via upload

---
+++ Module20/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Module20
+{
+    class Program
+    {
+        class User
+        {
+            public string Name { get; set; }
+            public string Login { get; set; }
+            public bool IsPremium { get; set; }
+
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            Console.WriteLine("Введите имя");
+            user.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите логин");
+            user.Login = Console.ReadLine();
+
+            Console.WriteLine("Премиум? да/нет");
+            string answ = Console.ReadLine();
+            switch (answ)
+            {
+                case "да":
+                    user.IsPremium = true;
+                    break;
+                case "нет":
+                    user.IsPremium = false;
+                    break;
+            }
+            if (user.IsPremium)
+            {
+                Console.WriteLine($"Приветсвуем, {user.Name}");
+            }
+            else ShowAds();
+        }
+    }
+}
